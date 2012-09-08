1f0bdf5504b3c0b9135ccc68721428a75b988e5f
Anders Finn <anders@visiblethinking.com>
2012-09-08 16:50:39 -0700

    Fixed Indentations

--- sbsdc.py
+++ sbsdc.py
@@ -32,46 +32,46 @@ else:
    REDIRECT_TO = "/dev/null"
    
 def createDaemon():
-    try:
-        pid = os.fork()
-    except OSError, e:
-        raise Exception, "%s [%d]" % (e.strerror, e.errno)
+   try:
+      pid = os.fork()
+   except OSError, e:
+      raise Exception, "%s [%d]" % (e.strerror, e.errno)
 
-    if (pid == 0):	# The first child.
-        os.setsid()
-        try:
-            pid = os.fork()	# Fork a second child.
-        except OSError, e:
-           raise Exception, "%s [%d]" % (e.strerror, e.errno)
+   if (pid == 0):	# The first child.
+      os.setsid()
+      try:
+         pid = os.fork()	# Fork a second child.
+      except OSError, e:
+         raise Exception, "%s [%d]" % (e.strerror, e.errno)
 
-        if (pid == 0):	# The second child.
-           os.chdir(WORKDIR) #chech to see if working directory exists
-           os.umask(UMASK)
-        else:
-           # exit() or _exit()?  See below.
-           os._exit(0)	# Exit parent (the first child) of the second child.
-    else:
-        os._exit(0)	# Exit parent of the first child.
+      if (pid == 0):	# The second child.
+         os.chdir(WORKDIR) #chech to see if working directory exists
+         os.umask(UMASK)
+      else:
+         # exit() or _exit()?  See below.
+         os._exit(0)	# Exit parent (the first child) of the second child.
+   else:
+      os._exit(0)	# Exit parent of the first child.
 
-    import resource		# Resource usage information.
-    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
-    if (maxfd == resource.RLIM_INFINITY):
-        maxfd = MAXFD
+   import resource		# Resource usage information.
+   maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+   if (maxfd == resource.RLIM_INFINITY):
+      maxfd = MAXFD
   
-    # Iterate through and close all file descriptors.
-    for fd in range(0, maxfd):
-        try:
-           os.close(fd)
-        except OSError:	# ERROR, fd wasn't open to begin with (ignored)
-           pass
-    os.open(REDIRECT_TO, os.O_RDWR)	# standard input (0)
+   # Iterate through and close all file descriptors.
+   for fd in range(0, maxfd):
+      try:
+         os.close(fd)
+      except OSError:	# ERROR, fd wasn't open to begin with (ignored)
+         pass
+   os.open(REDIRECT_TO, os.O_RDWR)	# standard input (0)
 
-    # Duplicate standard input to standard output and standard error.
-    os.dup2(0, 1)			# standard output (1)
-    os.dup2(0, 2)			# standard error (2)
-    open("test.log", "w").write("hello")
-    return(0)
+   # Duplicate standard input to standard output and standard error.
+   os.dup2(0, 1)			# standard output (1)
+   os.dup2(0, 2)			# standard error (2)
+   open("test.log", "w").write("hello")
+   return(0)
 
 if __name__ == "__main__":
-    retCode = createDaemon()
-    sys.exit(retCode)
+   retCode = createDaemon()
+   sys.exit(retCode)
