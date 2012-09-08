a9e6c098d29fac2fd9ba671ae601199472ba5bf4
Anders Finn <anders@visiblethinking.com>
2012-09-08 16:43:09 -0700

    Inital Commit

---
+++ sbsdc.py
@@ -0,0 +1,77 @@
+"""Smart Bus Stops Done Dirt Cheap
+
+Configurable daemon behaviors:
+
+
+"""
+
+__author__ = "Anders Finn and Cameron Jeffries"
+__copyright__ = "Copyright (C) 2012 Visible Thinking"
+
+__revision__ = "$Id$"
+__version__ = "0.1"
+
+# Standard Python modules.
+import os               # Miscellaneous OS interfaces.
+import sys              # System-specific parameters and functions.
+
+# Default daemon parameters.
+# File mode creation mask of the daemon.
+UMASK = 0
+
+# Default working directory for the daemon.
+WORKDIR = "/Users/anders/sbsdc"
+
+# Default maximum for the number of available file descriptors.
+MAXFD = 1024
+
+# The standard I/O file descriptors are redirected to /dev/null by default.
+if (hasattr(os, "devnull")):
+   REDIRECT_TO = os.devnull
+else:
+   REDIRECT_TO = "/dev/null"
+   
+def createDaemon():
+    try:
+        pid = os.fork()
+    except OSError, e:
+        raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+    if (pid == 0):	# The first child.
+        os.setsid()
+        try:
+            pid = os.fork()	# Fork a second child.
+        except OSError, e:
+           raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+        if (pid == 0):	# The second child.
+           os.chdir(WORKDIR) #chech to see if working directory exists
+           os.umask(UMASK)
+        else:
+           # exit() or _exit()?  See below.
+           os._exit(0)	# Exit parent (the first child) of the second child.
+    else:
+        os._exit(0)	# Exit parent of the first child.
+
+    import resource		# Resource usage information.
+    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+    if (maxfd == resource.RLIM_INFINITY):
+        maxfd = MAXFD
+  
+    # Iterate through and close all file descriptors.
+    for fd in range(0, maxfd):
+        try:
+           os.close(fd)
+        except OSError:	# ERROR, fd wasn't open to begin with (ignored)
+           pass
+    os.open(REDIRECT_TO, os.O_RDWR)	# standard input (0)
+
+    # Duplicate standard input to standard output and standard error.
+    os.dup2(0, 1)			# standard output (1)
+    os.dup2(0, 2)			# standard error (2)
+    open("test.log", "w").write("hello")
+    return(0)
+
+if __name__ == "__main__":
+    retCode = createDaemon()
+    sys.exit(retCode)
