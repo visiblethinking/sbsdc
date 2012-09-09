f0a0b7925b4a651622b2450e5cb035ed10301cc6
Anders Finn <anders@visiblethinking.com>
2012-09-08 18:42:46 -0700

    Now handles incomming twillio

--- sbsdc.py
+++ sbsdc.py
@@ -1,77 +1,49 @@
-"""Smart Bus Stops Done Dirt Cheap
-
-Configurable daemon behaviors:
-
-
-"""
-
-__author__ = "Anders Finn and Cameron Jeffries"
-__copyright__ = "Copyright (C) 2012 Visible Thinking"
-
-__revision__ = "$Id$"
-__version__ = "0.1"
-
-# Standard Python modules.
-import os               # Miscellaneous OS interfaces.
-import sys              # System-specific parameters and functions.
-
-# Default daemon parameters.
-# File mode creation mask of the daemon.
-UMASK = 0
-
-# Default working directory for the daemon.
-WORKDIR = "/Users/anders/sbsdc"
-
-# Default maximum for the number of available file descriptors.
-MAXFD = 1024
-
-# The standard I/O file descriptors are redirected to /dev/null by default.
-if (hasattr(os, "devnull")):
-   REDIRECT_TO = os.devnull
-else:
-   REDIRECT_TO = "/dev/null"
+# Echo server program
+import socket
+import sys
+import datetime
    
-def createDaemon():
-   try:
-      pid = os.fork()
-   except OSError, e:
-      raise Exception, "%s [%d]" % (e.strerror, e.errno)
+def get_location(location):
+    geo_lat="37.7750 N"
+    geo_long = "122.4183 W"  
+    return(geo_lat,geo_long)    
 
-   if (pid == 0):	# The first child.
-      os.setsid()
-      try:
-         pid = os.fork()	# Fork a second child.
-      except OSError, e:
-         raise Exception, "%s [%d]" % (e.strerror, e.errno)
-
-      if (pid == 0):	# The second child.
-         os.chdir(WORKDIR) #chech to see if working directory exists
-         os.umask(UMASK)
-      else:
-         # exit() or _exit()?  See below.
-         os._exit(0)	# Exit parent (the first child) of the second child.
-   else:
-      os._exit(0)	# Exit parent of the first child.
-
-   import resource		# Resource usage information.
-   maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
-   if (maxfd == resource.RLIM_INFINITY):
-      maxfd = MAXFD
-  
-   # Iterate through and close all file descriptors.
-   for fd in range(0, maxfd):
-      try:
-         os.close(fd)
-      except OSError:	# ERROR, fd wasn't open to begin with (ignored)
-         pass
-   os.open(REDIRECT_TO, os.O_RDWR)	# standard input (0)
-
-   # Duplicate standard input to standard output and standard error.
-   os.dup2(0, 1)			# standard output (1)
-   os.dup2(0, 2)			# standard error (2)
-   open("test.log", "w").write("hello")
-   return(0)
-
-if __name__ == "__main__":
-   retCode = createDaemon()
-   sys.exit(retCode)
+HOST = 'visiblethinking.com'                 # Symbolic name meaning the local host
+PORT = 13208               # Arbitrary non-privileged port
+s = None
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+    af, socktype, proto, canonname, sa = res
+    try:
+	s = socket.socket(af, socktype, proto)
+    except socket.error, msg:
+	s = None
+	continue
+    try:
+	s.bind(sa)
+	s.listen(1)
+    except socket.error, msg:
+	s.close()
+	s = None
+	continue
+    break
+if s is None:
+    print 'could not open socket'
+    sys.exit(1)
+conn, addr = s.accept()
+print 'Connected by', addr
+while 1:
+    data = conn.recv(1024)
+    if not data: break
+    references = data.split('&')
+    message = ""
+    sender = ""
+    for x in references:
+	if "body=" in x.lower():
+	    message = x[5:]
+	if "from=" in x.lower():
+	    sender = x[8:]
+    geo = message.split("+")[0]
+    geo = get_location(message.split(" ")[0])
+    message = " ".join(message.split("+")[1:])
+    open("sbsdc.log", "w").write("%s: %s | %s\n" % (datetime.datetime.now(), sender, message))
+conn.close()
