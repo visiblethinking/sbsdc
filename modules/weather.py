c4a224bfb7b426910b9dfed5a263a1dab87cd930
Andrew Hyder <andrew@urbanmapping.com>
2012-09-11 22:13:10 -0700

    Testing modules.

--- modules/weather.py
+++ modules/weather.py
@@ -15,28 +15,24 @@
 # keywords: weather, forecast
 #
 # # # # # # # # # # # # # # # # # # # # # #
-import sys
+import sys, urllib2
 args = sys.argv
 
+print 'Fuck You.'
 
-# An easy way to do it
-import requests
+# # An easy way to do it
+# r = urllib2.openurl('http://free.worldweatheronline.com/feed/weather.ashx?q=37.78,122.42&format=json&num_of_days=5&key=4e11214676001957120909')
 
-r = requests.get('http://free.worldweatheronline.com/feed/weather.ashx?q=37.78,122.42&format=json&num_of_days=5&key=4e11214676001957120909')
+# # Or using json module
+# import json
 
-weather_json = r.json
+# weather_text = r.text
+# weather_json = json.loads(weather_text)
 
-for day in weather_json['data']['weather']:
-	print day['date'] + ' ' + day['tempMaxF']
-	print day['weatherDesc'][0]['value']
+# for day in weather_json['data']['weather']:
+# 	print day['date'] + ' ' + day['tempMaxF']
+# 	print day['weatherDesc'][0]['value']
 
 
-# Or using json module
-import json
 
-weather_text = r.text
-weather_json = json.loads(weather_text)
 
-for day in weather_json['data']['weather']:
-	print day['date'] + ' ' + day['tempMaxF']
-	print day['weatherDesc'][0]['value']
