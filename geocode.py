4d0fff6d8302a480766e697479c2cf40b265a695
visiblethinking <anders@visiblethinking.com>
2012-09-11 22:13:28 -0700

    New Module sms code

--- geocode.py
+++ geocode.py
@@ -17,11 +17,11 @@ import urllib2, json
 
 # sub that does lookup of geolocation and retuns lat, long
 def get_location(bus_stop_id):
-	get_geo_url = 'http://ondrae.cartodb.com/api/v2/sql?q=SELECT latitude, longitude FROM sf_bus_stops WHERE stopid = ' + bus_stop_id
-    response = urllib2.openurl('get_geo_url')
+    get_geo_url = 'http://ondrae.cartodb.com/api/v2/sql?q=SELECT latitude, longitude FROM sf_bus_stops WHERE stopid = ' + bus_stop_id
+    response = urllib2.openurl(get_geo_url)
     for line in response:
     	json_response = json.loads(line)
     geo_lat = json_response['rows'][0]['latitude']
     geo_long = json_response['rows'][0]['longitude']
-    return(geo_lat,geo_long)
+    return([geo_lat,geo_long])
    
