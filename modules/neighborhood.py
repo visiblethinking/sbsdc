f856af39fba7dc4a6b90de58e08cb7cc56ea40b9
Andrew Hyder <andrew@urbanmapping.com>
2012-09-12 00:32:27 -0700

    Neighborhood mod.

---
+++ modules/neighborhood.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# # # # # # # # # # # # # # # # # # # # # # 
+#
+# project: Smart Bus Stops Done Dirt Cheap
+# title: neighborhood
+# file: neighborhood.py
+# description: Neighborhood module for SBSDC
+# language: python
+# 
+# authors: Andrew Hyder
+# date: 9/11/2012
+# version: 1.0.0
+# notes: fetches and sends latest weather forecast
+#
+# keywords: weather, forecast
+#
+# # # # # # # # # # # # # # # # # # # # # #
+import sys, urllib, simplejson, datetime
+lat = sys.argv[1]
+lon = sys.argv[2]
+message = sys.argv[3]
+
+get_hood_url = 'http://query.mapfluence.com/2.0/faa2473800838e5a0b407b40b3ab93ed/spatialquery.json?from=umi.neighborhoods.geometry&select=name'
+get_hood_url = get_hood_url + '&where=intersects({%22type%22:%22Point%22,%22coordinates%22:['+str(lon)+','+str(lat)+'37.748]})'
+
+response = urllib.urlopen(get_hood_url)
+for line in response:
+	response_dict = simplejson.loads(line)
+
+for feature in response_dict['features']:
+	print feature['properties']['name']
