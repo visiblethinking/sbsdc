7a21252214ee9a0c04d10ac602d0b772ab239fad
Andrew Hyder <andrew@urbanmapping.com>
2012-09-13 21:20:08 -0700

    lolcaaaaat humble beginnings

---
+++ modules/lolcat.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# # # # # # # # # # # # # # # # # # # # # # 
+#
+# project: Smart Bus Stops Done Dirt Cheap
+# title: lolcat
+# file: lolcat.py
+# description: LOLCAT
+# language: python
+# 
+# authors: Andrew Hyder
+# date: 9/9/2012
+# version: 1.0.0
+# notes: LOLCAT
+#
+# keywords: LOLCAT
+#
+# # # # # # # # # # # # # # # # # # # # # #
+import sys
+lat = sys.argv[1]
+lon = sys.argv[2]
+message = sys.argv[3]
+
+
+print '''
+\`*-.                    
+ )  _`-.                 
+.  : `. .                
+: _   '  \               
+; *` _.   `*-._          
+`-.-'          `-.       
+  ;       `       `.     
+  :.       .        \    
+  . \  .   :   .-'   .   
+  '  `+.;  ;  '      :   
+  :  '  |    ;       ;-. 
+  ; '   : :`-:     _.`* ;
+.*' /  .*' ; .*`- +'  `*' 
+`*-*   `*-*  `*-*'
+'''
