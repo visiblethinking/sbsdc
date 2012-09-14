35ce951c151ba6b35a9d5a788a90524887b80949
Andrew Hyder <andrew@urbanmapping.com>
2012-09-13 21:46:57 -0700

    Fixing neighborhood

--- modules/neighborhood.py
+++ modules/neighborhood.py
@@ -21,7 +21,7 @@ lon = sys.argv[2]
 message = sys.argv[3]
 
 get_hood_url = 'http://query.mapfluence.com/2.0/faa2473800838e5a0b407b40b3ab93ed/spatialquery.json?from=umi.neighborhoods.geometry&select=name'
-get_hood_url = get_hood_url + '&where=intersects({%22type%22:%22Point%22,%22coordinates%22:['+str(lon)+','+str(lat)+']})'
+get_hood_url = get_hood_url + '&where=intersects({%22type%22:%22Point%22,%22coordinates%22:['+str(lat)+','+str(lon)+']})'
 
 response = urllib.urlopen(get_hood_url)
 for line in response:
