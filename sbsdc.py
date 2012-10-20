66f5a95f2ff29b9aaccd72b341bb6221f5eac18e
Cameron Jeffries <camservo@gmail.com>
2012-10-20 06:33:52 -0700

    changed about settings in sbsdc.py

--- sbsdc.py
+++ sbsdc.py
@@ -15,9 +15,8 @@
     
 def get_module_name(message_list):
 	# TODO: remove punctuation
-    for message_word in message_list:
-	if str(message_word).lower() == 'about':
-	    return 'aboutprojects'
+    if message_list[0].lower() == 'about':
+	return 'aboutprojects'
     for module_name, keywords in module_keys.items():
         keywords = keywords.split(', ')
         for keyword in keywords:
